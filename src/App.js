ae2fee512f745aef9d214550338c1679b1aa5ac9
rafael-java <rafael23.labomar@gmail.com>
2022-03-10 19:08:51 -0300

    Novas alterações do Júlio

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
-import logo from "./logo.svg";
+import img1 from "./assets/img1.jpg";
+import img2 from "./assets/img2.jpg";
+import img3 from "./assets/img3.jpg";
+import img4 from "./assets/img4.jpg";
+import img5 from "./assets/img5.jpg";
+import img6 from "./assets/img6.jpg";
+import img7 from "./assets/img7.jpg";
+import img8 from "./assets/img8.jpg";
+import img9 from "./assets/img9.jpg";
+
 import "./App.css";
 import { Button } from "react-bootstrap";
 
@@ -52,15 +61,40 @@ function App() {
 
   function setFieldObj(line, col, setter, posiblity) {
     let newPositiions = [...posiblity];
-    newPositiions[line][col] = String(choosed);
-    setter(newPositiions);
+    let isValid = validateSeted(newPositiions);
+    if (isValid) {
+      newPositiions[line][col] = String(choosed);
+      setChoosed(0);
+      setter(newPositiions);
+    } else {
+      if (newPositiions[line][col] == String(choosed)) {
+        newPositiions[line][col] = "";
+        setChoosed(0);
+        setter(newPositiions);
+      }
+      console.log("Choosed nao Vallido para ser setado nesse Tabuleiro.");
+    }
   }
 
-  function validateSeted() {
+  // |1|2|3|7|X|6|8|5|9
+  function transformTabuleiro(tabuleiro) {
+    let TabuleiroArray = tabuleiro.split("|");
+    let MatrizTabuleiro = [
+      TabuleiroArray.splice(0, 3),
+      TabuleiroArray.splice(0, 3),
+      TabuleiroArray.splice(0, 3),
+    ];
+    return MatrizTabuleiro;
+  }
+
+  function validateSeted(toValidate) {
     let valid = true;
-    obj.forEach((item) => {
-      item.forEach((position) => {});
+    toValidate.forEach((item) => {
+      item.forEach((position) => {
+        if (position == choosed) valid = false;
+      });
     });
+    return valid;
   }
 
   return (
@@ -82,8 +116,8 @@ function App() {
       <body>
         {/* <div style={{position:'absolute', top:10, left:10,backgroundColor:'red',width:90,height:90,justifyContent:'center',textAlign:'center'}}><RiNumber1 size={90} style={{alignSelf:'center'}}/></div> */}
 
-        <table>
-          Jogo
+        <table style={{ left: "40vh" }}>
+          Inicial
           {positions.map((item, lin) => (
             <tr>
               {item.map((position, col) => (
@@ -97,8 +131,8 @@ function App() {
           ))}
         </table>
 
-        <table style={{ position: "absolute", left: "60vh" }}>
-          Config...
+        <table style={{ position: "absolute", left: "90vh" }}>
+          Final
           {obj.map((item, lin) => (
             <tr>
               {item.map((position, col) => (
@@ -110,55 +144,80 @@ function App() {
           ))}
         </table>
         <Button style={{ position: "absolute" }}>Text</Button>
-        <table style={{ top: "75vh" }}>
-          <th
-            style={choosed === 1 ? { background: "green" } : {}}
-            onClick={() => setChoosed(1)}
-          >
-            1
-          </th>
-          <th
-            style={choosed === 2 ? { background: "green" } : {}}
-            onClick={() => setChoosed(2)}
-          >
-            2
-          </th>
-          <th
-            style={choosed === 3 ? { background: "green" } : {}}
-            onClick={() => setChoosed(3)}
-          >
-            3
-          </th>
-          <th
-            style={choosed === 4 ? { background: "green" } : {}}
-            onClick={() => setChoosed(4)}
-          >
-            4
-          </th>
-          <th
-            style={choosed === 5 ? { background: "green" } : {}}
-            onClick={() => setChoosed(5)}
-          >
-            5
-          </th>
-          <th
-            style={choosed === 6 ? { background: "green" } : {}}
-            onClick={() => setChoosed(6)}
-          >
-            6
-          </th>
-          <th
-            style={choosed === 7 ? { background: "green" } : {}}
-            onClick={() => setChoosed(7)}
-          >
-            7
-          </th>
-          <th
-            style={choosed === 8 ? { background: "green" } : {}}
-            onClick={() => setChoosed(8)}
-          >
-            8
-          </th>
+        <table style={{}}>
+          Escolha
+          <tr>
+            <th
+              style={choosed === 1 ? { background: "green" } : {}}
+              onClick={() => setChoosed(1)}
+            >
+              <img src={img1} />
+            </th>
+            <th
+              style={choosed === 5 ? { background: "green" } : {}}
+              onClick={() => setChoosed(5)}
+            >
+              <img src={img5} />
+            </th>
+          </tr>
+          <tr>
+            <th
+              style={choosed === 2 ? { background: "green" } : {}}
+              onClick={() => setChoosed(2)}
+            >
+              <img src={img2} />
+            </th>
+            <th
+              style={choosed === 6 ? { background: "green" } : {}}
+              onClick={() => setChoosed(6)}
+            >
+              <img src={img6} />
+            </th>
+          </tr>
+          <tr>
+            <th
+              style={choosed === 3 ? { background: "green" } : {}}
+              onClick={() => setChoosed(3)}
+            >
+              <img src={img3} />
+            </th>
+            <th
+              style={choosed === 7 ? { background: "green" } : {}}
+              onClick={() => setChoosed(7)}
+            >
+              <img src={img7} />
+            </th>
+          </tr>
+          <tr>
+            <th
+              style={choosed === 4 ? { background: "green" } : {}}
+              onClick={() => setChoosed(4)}
+            >
+              <img src={img4} />
+            </th>
+            <th
+              style={choosed === 8 ? { background: "green" } : {}}
+              onClick={() => setChoosed(8)}
+            >
+              <img src={img8} />
+            </th>
+          </tr>
+          <tr>
+            <th
+              style={choosed === 9 ? { background: "green" } : {}}
+              onClick={() => setChoosed(9)}
+            >
+              <img src={img9} />
+            </th>
+          </tr>
+        </table>
+
+        <table style={{ top: "65vh", left: "50vw" }}>
+          <tr>
+            <th>Abertos</th>
+            <th>Fechados</th>
+            <th>X</th>
+          </tr>
         </table>
       </body>
     </div>
